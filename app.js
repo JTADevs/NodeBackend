a0f22ed67f4abacddd5771b9e90471fa9aa103d3
Kubot <jakub-kurzacz@wp.pl>
2024-05-03 12:46:03 +0200

    asd

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const http = require('http');
+var http = require('http');
 
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
- res.statusCode = 200;
- res.setHeader('Content-Type', 'text/plain');
- res.end('Hello, World!');
-}).listen(PORT, err => {
- if (err) {
-   console.error(`Error starting server: ${err}`);
-   process.exit(1);
- }
- console.log(`Server running on port ${PORT}`);
-});
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
