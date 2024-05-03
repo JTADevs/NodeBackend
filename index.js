351096d2cc97fda591a8e519861fc6163759209e
Kubot <jakub-kurzacz@wp.pl>
2024-05-03 22:24:37 +0200

    qqqq

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var http = require('http');
 var url = require('url');
+var mysql = require('mysql');
 
-// Create a server object:
+// Konfiguracja połączenia z bazą danych
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', // Hasło do bazy danych
+  database: 'shooterapp'
+});
+
+// Połączenie z bazą danych
+connection.connect(function(err) {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych:', err.stack);
+    return;
+  }
+  console.log('Połączenie z bazą danych zostało nawiązane.');
+});
+
+// Utworzenie serwera HTTP
 http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true);
   var pathname = parsedUrl.pathname;
@@ -10,41 +28,23 @@ http.createServer(function (req, res) {
   if (req.method === 'GET') {
     // Obsługa podstrony "/"
     if (pathname === '/') {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>Strona główna</h1>');
-      res.end();
-    }
-    // Obsługa podstrony "/about"
-    else if (pathname === '/about') {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>O nas</h1>');
-      res.end();
-    }
-    // Obsługa podstrony "/contact"
-    else if (pathname === '/contact') {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>Kontakt</h1>');
-      res.end();
-    }
-    // Obsługa nieznanej podstrony
-    else {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write('<h1>404 Not Found</h1>');
-      res.end();
-    }
-  }
-  // Obsługa żądań POST
-  else if (req.method === 'POST') {
-    // Obsługa podstrony "/submit"
-    if (pathname === '/submit') {
-      var body = '';
-      req.on('data', function (data) {
-        body += data;
-      });
-      req.on('end', function () {
-        console.log('Received POST data:', body);
+      // Zapytanie do bazy danych
+      connection.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) {
+          console.error('Błąd zapytania do bazy danych:', error);
+          res.writeHead(500, {'Content-Type': 'text/html'});
+          res.write('<h1>500 Internal Server Error</h1>');
+          res.end();
+          return;
+        }
+        // Wyświetlanie danych z bazy na stronie
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>Dane zostały wysłane metodą POST</h1>');
+        res.write('<h1>Strona główna</h1>');
+        res.write('<ul>');
+        results.forEach(function(user) {
+          res.write('<li>Email: ' + user.email + ', Hasło: ' + user.password + '</li>');
+        });
+        res.write('</ul>');
         res.end();
       });
     }
@@ -55,4 +55,4 @@ http.createServer(function (req, res) {
       res.end();
     }
   }
-}).listen(80); // The server object listens on port 80
+}).listen(80); // Serwer nasłuchuje na porcie 80
