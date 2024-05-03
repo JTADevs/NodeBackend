152c2864ce72b361eaa846f8a9de0d319fcc2d41
Kubot <jakub-kurzacz@wp.pl>
2024-05-03 22:06:32 +0200

    asd

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 var http = require('http');
+var mysql = require('mysql');
 
-//create a server object:
+// Tworzenie połączenia z bazą danych
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'shooterapp'
+});
+
+// Nawiązywanie połączenia
+connection.connect();
+
+// Tworzenie serwera HTTP
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+  // Obsługa żądania GET na podstronie /get
+  if (req.url === '/get') {
+    // Wykonaj zapytanie SQL do pobrania danych z bazy danych
+    connection.query('SELECT * FROM users', function (error, results, fields) {
+      if (error) throw error;
+      // Wysyłanie danych w formie odpowiedzi
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(results));
+      res.end();
+    });
+  } else {
+    // Wysyłanie pustej odpowiedzi dla innych żądań
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end();
+  }
+}).listen(80);
+
+// Zamykanie połączenia z bazą danych po zamknięciu serwera
+process.on('SIGINT', function() {
+  connection.end();
+  process.exit();
+});
