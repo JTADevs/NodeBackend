80ccb1703d8695eca9faa42dfe1d96766b0743e4
Kubot <jakub-kurzacz@wp.pl>
2024-05-04 13:38:54 +0200

    a

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ http.createServer(function (req, res) {
   connection.connect(function (err) {
     if (err) {
         console.log("Error in the connection")
-        console.log(err)
+        //console.log(err)
     } else {
       console.log(`Database Connected`)
       connection.query(`SHOW DATABASES`,
         function (err, result) {
           if (err) {
-            console.log(`Error executing the query - ${err}`)
+            console.log(`Error executing the query`)
+            //console.log(`Error executing the query - ${err}`)
             res.write('jd1');
           } else {
             console.log("Result: ", result)
