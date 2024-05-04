048a9b897bfd5b55560c5d0d20413d74fb6961a7
Kubot <jakub-kurzacz@wp.pl>
2024-05-04 15:54:59 +0200

    s

--- index.js
+++ index.js
@@ -28,6 +28,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Result');
   res.end();
-}).listen(8080);
-
-console.log('Server running at http://localhost:8080/');
+}).listen(80);
