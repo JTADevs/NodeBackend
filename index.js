dbf83301258e5939e84615781254fbb4f50f9ee1
Kubot <jakub-kurzacz@wp.pl>
2024-05-05 19:07:27 +0200

    z

--- index.js
+++ index.js
@@ -90,9 +90,11 @@ const mysql = require('mysql');
 const app = express();
 
 // Ustawienia CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Adres Twojej aplikacji React
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
