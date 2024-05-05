a6559a1b5ccb27c054cb6adeb366b20494053fa8
Kubot <jakub-kurzacz@wp.pl>
2024-05-05 19:15:18 +0200

    x

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const app = express();
 
 // Ustawienia CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
