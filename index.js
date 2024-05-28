1da05c41089dae3134e95e2fc7edeee8d6d7ab5f
Kubot <jakub-kurzacz@wp.pl>
2024-05-28 18:31:42 +0200

    asd

--- index.js
+++ index.js
@@ -1,88 +1,3 @@
-// const http = require('http');
-// const url = require('url');
-// const mysql = require('mysql');
-
-// // Tworzenie połączenia z bazą danych
-// var conn = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "zaq1@WSX",
-//   database: "shooterapp"
-// });
-
-// // Nawiązywanie połączenia z bazą danych
-// conn.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected to MySQL database!");
-// });
-
-// // Tworzenie serwera HTTP
-// http.createServer(function (req, res) {
-//   const parsedUrl = url.parse(req.url, true);
-
-//   if (req.method === 'POST') {
-//     let body = '';
-//     req.on('data', function(chunk) {
-//         body += chunk.toString();
-//     });
-
-//     req.on('end', function() {
-//       if (parsedUrl.pathname === '/get') {
-//         conn.query("SELECT * FROM account", function (err, result, fields) {
-//           if (err) {
-//             console.error(err);
-//             res.writeHead(500, {'Content-Type': 'text/plain'});
-//             res.end('Internal Server Error');
-//             return;
-//           }
-          
-//           res.writeHead(200, {'Content-Type': 'application/json'});
-//           res.end(JSON.stringify(result));
-//         });
-//       } else if (parsedUrl.pathname === '/register') {
-//         const postData = JSON.parse(body);
-
-//         const newRecord = {
-//           email: postData.email,
-//           password: postData.password
-//         };
-
-//         const sqlQuery = 'INSERT INTO account (email, password) VALUES (?, ?)';
-//         conn.query(sqlQuery, [newRecord.email, newRecord.password], function (err, result) {
-//           if (err) {
-//             console.error(err);
-//             res.writeHead(500, {'Content-Type': 'text/plain'});
-//             res.end('Internal Server Error');
-//             return;
-//           }
-
-//           res.writeHead(200, {'Content-Type': 'text/plain'});
-//           res.end('New record added successfully');
-//         });
-//       } else if (parsedUrl.pathname === '/login') {
-
-//       }
-//     });
-//   } else if (req.method === 'GET') {
-//     conn.query("SELECT * FROM account", function (err, result, fields) {
-//       if (err) {
-//           console.error(err);
-//           res.writeHead(500, {'Content-Type': 'text/plain'});
-//           res.end('Internal Server Error');
-//           return;
-//       }
-      
-//       res.writeHead(200, {'Content-Type': 'application/json'});
-//       res.end(JSON.stringify(result));
-//     });
-//   } else {
-//     res.writeHead(405, {'Content-Type': 'text/plain'});
-//     res.end('Method Not Allowed');
-//   }
-// }).listen(80);
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
@@ -103,13 +18,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Połączenie z bazą danych
+// const conn = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "zaq1@WSX",
+//   database: "shooterapp"
+// });
 const conn = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "zaq1@WSX",
-  database: "shooterapp"
+  password: "",
+  database: "bike_bazar"
 });
+
 
 // Nawiązanie połączenia z bazą danych
 conn.connect(function(err) {
@@ -118,8 +41,8 @@ conn.connect(function(err) {
 });
 
 // Obsługa żądań GET dla /get
-app.get('/get', (req, res) => {
-  conn.query("SELECT * FROM account", function (err, result, fields) {
+app.get('/announcement/getAll', (req, res) => {
+  conn.query("SELECT * FROM announcements", function (err, result, fields) {
     if (err) {
       console.error(err);
       res.status(500).send('Internal Server Error');
@@ -127,6 +50,36 @@ app.get('/get', (req, res) => {
     }
     console.log(result);
     res.status(200).json(result);
+  });
+});
+
+// Obsługa żądań GET dla /get
+app.get('/announcement/getByUser', (req, res) => {
+  const { user_uid } = req.query;
+  conn.query("SELECT * FROM announcements WHERE user_uid = ?", [user_uid], function (err, result, fields) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    console.log(result);
+    res.status(200).json(result);
+  });
+});
+
+// Obsługa żądań POST dla /addAnnouncement
+app.post('/announcement/add', (req, res) => {
+  const { user_uid, title, description, price } = req.body;
+
+  const sqlQuery = 'INSERT INTO announcements (user_uid, title, description, price) VALUES (?, ?, ?, ?)';
+  conn.query(sqlQuery, [user_uid, title, description, price], function (err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    console.log('New record added successfully');
+    res.status(200).send('New record added successfully');
   });
 });
 
@@ -146,6 +99,22 @@ app.post('/register', (req, res) => {
   });
 });
 
+// Obsługa żądań POST dla /register
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const sqlQuery = 'SELECT * FROM account WHERE email = ? AND password = ?';
+  conn.query(sqlQuery, [email, password], function (err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    console.log('New record added successfully');
+    res.status(200).send('New record added successfully');
+  });
+});
+
 // Obsługa innych typów żądań
 // app.all('*', (req, res) => {
 //   console.log('Method Not Allowed');
@@ -153,6 +122,6 @@ app.post('/register', (req, res) => {
 // });
 
 // Uruchomienie serwera na porcie 80
-const server = app.listen(80, () => {
+const server = app.listen(90, () => {
   console.log(`Server is running on port ${server.address().port}`);
 });
